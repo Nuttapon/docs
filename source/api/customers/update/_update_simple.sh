59c0638a267e567d363dfb8e4970809701c87d81
zdk <di@omise.co>
2015-03-02 10:02:06 +0700

    Revert file to the previous version

--- source/api/customers/update/_update_simple.sh
+++ source/api/customers/update/_update_simple.sh
@@ -1,3 +1,5 @@
-omise.customers.update('cust_test_4xtrb759599jsxlhkrb',
-{ 'email': 'john.smith@example.com',
-  'description': 'Another description' }, function(error, customer) {});
+curl https://api.omise.co/customers/cust_test_4xtrb759599jsxlhkrb \
+  -X PATCH \
+  -u skey_test_4xsjvwfnvb2g0l81sjz: \
+  -d "email=john.smith@example.com" \
+  -d "description=Another description"
