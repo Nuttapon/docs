ecdea9d86a0abcb90a7050667196d38a812aa5ab
naoki <naoki@alpha-do.com>
2015-06-05 14:05:17 +0900

    javaのdocsを追加

--- source/api/recipients/create/_create.java
+++ source/api/recipients/create/_create.java
@@ -1,1 +1,14 @@
-// Coming soon
+import java.util.HashMap;
+
+import co.omise.model.Recipient;
+
+Recipient recipient = Recipient.create(new HashMap<String, Object>() {
+  {put("name", "Somchai Prasert");}
+  {put("email", "somchai.prasert@example.com");}
+  {put("type", "individual");}
+  {put("bank_account", new HashMap<String, Object>() {
+    {put("brand", "bbl");}
+    {put("number", "1234567890");}
+    {put("name", "SOMCHAI PRASERT");}
+  });}
+});
--- source/api/recipients/retrieve/_retrieve.java
+++ source/api/recipients/retrieve/_retrieve.java
@@ -1,1 +1,3 @@
-// Coming soon
+import co.omise.model.Recipient;
+
+Recipient recipient = Recipient.retrieve("recp_test_4z6p7e0m4k40txecj5o");
