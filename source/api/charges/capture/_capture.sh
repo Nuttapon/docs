707aca7dd9cbb467d20228ff9208694eb6d59499
piano <piano@omise.co>
2015-09-08 19:19:12 +0700

    improved curl scripts, remove unneccessary slashes :dancers:

--- source/api/charges/capture/_capture.sh
+++ source/api/charges/capture/_capture.sh
@@ -1,3 +1,3 @@
 curl https://api.omise.co/charges/chrg_test_4xso2s8ivdej29pqnhz/capture \
-  -X POST
-  -u skey_test_4xsjvwfnvb2g0l81sjz: \
+  -X POST \
+  -u skey_test_4xsjvwfnvb2g0l81sjz:
--- source/api/disputes/update/_update.sh
+++ source/api/disputes/update/_update.sh
@@ -1,4 +1,4 @@
 curl https://api.omise.co/disputes/dspt_test_4zei9tuoblpoxid97xp \
   -X PATCH \
   -u skey_test_4xsjvwfnvb2g0l81sjz: \
-  -d "message=Unauthorized transaction" \
+  -d "message=Unauthorized transaction"
